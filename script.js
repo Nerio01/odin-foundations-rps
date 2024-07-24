a5226ab00b2ef3b591a9cc3ccad39e1ef9b23fc0
Nerio01 <nerios.cave@gmail.com>
2024-07-24 08:11:23 +0000

    added first iteration of pseudocode to script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+//
+// get user name
+// accept user input and store it in name constant variable 
+// output greeting of the user by name
+// output rules of the game to the user
+// IF number of rounds is > 3 THEN stop the game and output the winner name and score ELSE continue
+// start round 
+// get randomized computer gesture stored in a variable currentCompAnswer 
+// accept user input and store it in currentUserAnswer variable
+// normalize and validate user input stored in currentUserAnswer 
+// IF user input validated THEN compare against currentCompAnswer 
+// IF user wins, output win response THEN start new round 
+// IF user loses, output lose responce THEN start new round 
+// 
