695bd97e443b11383d80fd39367397fc1c917c57
Nerio01 <nerios.cave@gmail.com>
2024-07-24 08:40:07 +0000

    started writing supporting fuctions (not completed yet)

--- script.js
+++ script.js
@@ -7,8 +7,39 @@
 // start round 
 // get randomized computer gesture stored in a variable currentCompAnswer 
 // accept user input and store it in currentUserAnswer variable
-// normalize and validate user input stored in currentUserAnswer 
+// validate THEN normalize user input stored in currentUserAnswer 
 // IF user input validated THEN compare against currentCompAnswer 
-// IF user wins, output win response THEN start new round 
-// IF user loses, output lose responce THEN start new round 
+// IF draw THEN start new round
+// IF user wins THEN output win response THEN start new round 
+// IF user loses THEN output lose responce THEN start new round 
 // 
+let roundCount = 0; // round counting variable to be later stored inside the main function, max is 3 round.  
+const handCombinations = ['rock', 'paper', 'scissors'];
+const [rock, paper, scissors] = handCombinations;
+
+// const getCompHand = () => {
+//     const 
+// }
+
+const compareHands = (userHand, compHand) => {
+
+    if (userHand === compHand) {
+        roundCount += 1;
+        console.log(`DRAW!
+        Starting next round!`);
+        return 'draw';
+    } 
+}
+
+console.log(compareHands('rock', 'rock'));
+
+// COMBINATIONS 
+// ROCK vs SCISSORS - ROCK WIN 
+// ROCK vs PAPER - ROCK LOSE  
+// ROCK vs ROCK - DRAW
+// SCISSORS vs ROCK - SCISSORS LOSE
+// SCISSORS vs PAPER - SCISSORS WIN 
+// SCISSORS vs SCISSORS - DRAW
+// PAPER vs ROCK - PAPER WINS
+// PAPER vs SCISSORS - PAPER LOSE
+// PAPER VS PAPER - DRAW
