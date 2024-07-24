dc2eb8c3c9f86490b1028829438160d8c6dd50b6
Nerio01 <nerios.cave@gmail.com>
2024-07-24 09:26:52 +0000

    user input validation function and hand comparison fuctions are working

--- script.js
+++ script.js
@@ -14,24 +14,64 @@
 // IF user loses THEN output lose responce THEN start new round 
 // 
 let roundCount = 0; // round counting variable to be later stored inside the main function, max is 3 round.  
-const handCombinations = ['rock', 'paper', 'scissors'];
-const [rock, paper, scissors] = handCombinations;
+const [rock, paper, scissors] = ['rock', 'paper', 'scissors'];
 
 // const getCompHand = () => {
 //     const 
 // }
 
+const validateUserInput = (userInput) => {
+    const lowerCaseInput = userInput.toLowerCase();
+    if (lowerCaseInput !== rock 
+    && lowerCaseInput !== paper 
+    && lowerCaseInput !== scissors) {
+        return 'invalid';
+        // here we should restart the round without changing roundCount value - UPDATE - NVM not required as this will be done inside the main function
+    }
+    return 'valid';
+}
+
+console.log('Testing user input validation - case of invalid input');
+console.log(validateUserInput('fist'));
+console.log('Testing user input validation - case of valid input');
+console.log(validateUserInput('paper'));
+
 const compareHands = (userHand, compHand) => {
 
+    let winner = '';
     if (userHand === compHand) {
         roundCount += 1;
         console.log(`DRAW!
         Starting next round!`);
         return 'draw';
     } 
-}
 
+    switch (true) {
+        case ((userHand === rock) && (compHand === scissors)):
+        case ((userHand === paper) && (compHand === rock)):
+        case ((userHand === scissors) && (compHand === paper)):
+            winner = 'user';
+            roundCount += 1;
+            break;
+        case ((userHand === rock) && (compHand === paper)):
+        case ((userHand === paper) && (compHand === scissors)):
+        case ((userHand === scissors) && (compHand === rock)):
+            winner = 'computer';
+            roundCount += 1;
+            break;
+    }
+
+    return `Winner: ${winner}!`;
+};
+console.log('Testing hand comparison function - case of identical hands');
 console.log(compareHands('rock', 'rock'));
+
+console.log('Testing hand comparison function - case of user win');
+console.log(compareHands('rock', 'scissors'));
+
+console.log('Testing hand comparison function - case of computer win');
+console.log(compareHands('paper', 'scissors'));
+
 
 // COMBINATIONS 
 // ROCK vs SCISSORS - ROCK WIN 
