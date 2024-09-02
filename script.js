4296f470dfbfb26fb77f416a673f82abdf091ad6
Nerio_Ozran <nerios.cave@gmail.com>
2024-09-02 22:57:51 +0300

    removed user input validation because it now ties to buttons, also added running score

--- script.js
+++ script.js
@@ -17,16 +17,6 @@ const generateHand = () => {
     return validHands[getRandomIntInclusive(0,2)];
 }
 
-const validateUserInput = (userInput) => {
-    const lowerCaseInput = userInput.toLowerCase();
-    if (lowerCaseInput !== rock 
-    && lowerCaseInput !== paper 
-    && lowerCaseInput !== scissors) {
-        return 'invalid';
-    }
-    return 'valid';
-}
-
 const compareHands = (userHand, compHand) => {
 
     let winner = '';
@@ -48,60 +38,44 @@ const compareHands = (userHand, compHand) => {
 
     return winner;
 };
-//const getHumanChoice = () => prompt("Enter rock, paper or scissors: ");
+
+const currentScore = {
+  player: 0,
+  computer: 0,
+}
+
+const updateScore = (roundWinner) => {
+  if (roundWinner === 'computer') {
+    currentScore.computer += 1;
+  }
+  if (roundWinner === 'user') {
+    currentScore.player += 1;
+  }
+}
+
+const resetScore = () => {
+  currentScore.player = 0;
+  currentScore.computer = 0;  
+}
+
+const updateBoardScore = () => {
+  const playerScore = document.querySelector('div .player-score');
+  const computerScore = document.querySelector('div .computer-score');
+  playerScore.textContent = currentScore.player;
+  computerScore.textContent = currentScore.computer;
+}
+
 const playRound = (userhand) => {
     const humanSelection = userhand;
     const computerSelection = generateHand();
-
-    if (validateUserInput(humanSelection) === `invalid`) {
-        console.log('Invalid hand. Please enter "rock", "paper" or "scissors".');
-        return;
-    }
-    const winner = compareHands(humanSelection, computerSelection); 
-    const output = document.querySelector('.output');
-    output.textContent = winner;
-    return winner;
+    const roundWinner = compareHands(humanSelection, computerSelection); 
+    const output = document.querySelector('.winner');
+    updateScore(roundWinner);
+    console.log(currentScore);
+    updateBoardScore();
+    output.textContent = roundWinner;
+    return roundWinner;
 }
 
 
-//const playGame = () => {
-//    console.log('Welcome to Rock Paper Scissors game!');
-//    const userName = prompt('What is your name: ');
-//    console.log(`Hello, ${userName}!`);
-//    console.log('This game will consist of three rounds. Lets start!');
-//    let userScore = 0;
-//    let computerScore = 0;
-//    let rounds = 1;
-//    while (rounds <= 5) {
-//        const result = playRound();
-//        if (result === 'computer') {
-//            computerScore += 1;
-//            rounds += 1;
-//        }
-//        if (result === 'user') {
-//            userScore += 1;
-//            rounds += 1;
-//        }
-//        if (result === 'draw') {
-//            rounds += 1;
-//        }
-//        if (result === 'draw') {
-//            console.log('Draw!');
-//        }
-//        console.log (`User: ${userScore}, Computer: ${computerScore}`);
-//    }
-//
-//    console.log (`User: ${userScore}, Computer: ${computerScore}`);
-//    if (userScore === computerScore) {
-//        console.log('The game ends with DRAW!');
-//        return;
-//    }
-//    if (userScore > computerScore) {
-//        console.log ('You win!');
-//        return;
-//    }
-//    console.log('You lost!');
-//    return;
-//};
-//
-    
+   
